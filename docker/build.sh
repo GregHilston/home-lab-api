a206b4b795b6111f553c1cff2cde190ccfe58b4e
GregHilston <Gregory.Hilston@gmail.com>
2020-02-19 07:12:52 -0700

    woops forgot to commit this last night, fixed docker container and added routes that I'm going to remove, as we can just ssh when the homelab is on

--- docker/build.sh
+++ docker/build.sh
@@ -4,4 +4,10 @@ set -e
 SCRIPT_DIR=$(dirname "$0")
 . $SCRIPT_DIR/config.sh
 
-docker build -f docker/Dockerfile -t $service .
+echo "[*] copying files to $PWD for build"
+cp ../requirements.txt .
+
+docker build -t $service .
+
+echo "[*] cleaning up files needed for build"
+rm ./requirements.txt
--- docker/config.sh
+++ docker/config.sh
@@ -2,3 +2,5 @@
 set -e
 
 service=home-lab-api
+appPort=1337
+externalPort=1337
